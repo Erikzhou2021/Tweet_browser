dcf3a4b37273bcc041769c8d2d24aacd6b7d3535
erzh <erzh@umich.edu>
2022-12-02 11:17:26 -0500

    finished clustering

--- tests/tweet_tester.py
+++ tests/tweet_tester.py
@@ -12,10 +12,22 @@ import random
 import tracemalloc
 import re
 from enum import Enum
-
-# clear 'temp' folder every week
-import os, sys, time
-from subprocess import call
+import string
+import scipy
+import sklearn
+import nltk
+import plotly.express as px
+from nltk.stem import PorterStemmer
+#nltk.download('stopwords')
+#from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import TruncatedSVD
+from scipy.sparse.csc import csc_matrix
+import umap
+from sklearn.mixture import GaussianMixture
+from sklearn.cluster import KMeans
+#import hdbscan
+from sklearn.neighbors import kneighbors_graph
 
 # Ignore warnings
 import warnings
@@ -24,6 +36,7 @@ import warnings
 
 #from formatter import NullFormatter
 warnings.filterwarnings("ignore")
+
 
 # this function reads in the data (copied from online)
 def parse_data(filename):
@@ -42,6 +55,30 @@ def parse_data(filename):
         print(e)
         return
     return df
+
+def preProcessingFcn(tweet, removeWords=list(), stem=True, removeURL=True, removeStopwords=True, 
+    removeNumbers=False, removePunctuation=True):
+    """
+    Cleans tweets by removing words, stemming, etc.
+    """
+    ps = PorterStemmer()
+    tweet = tweet.lower()
+    tweet = re.sub(r"\\n", " ", tweet)
+    tweet = re.sub(r"&amp", " ", tweet)
+    if removeURL==True:
+        tweet = re.sub(r"http\S+", " ", tweet)
+    if removeNumbers==True:
+        tweet=  ''.join(i for i in tweet if not i.isdigit())
+    if removePunctuation==True:
+        for punct in string.punctuation:
+            tweet = tweet.replace(punct, ' ')
+    #if removeStopwords==True:
+        #tweet = ' '.join([word for word in tweet.split() if word not in stopwords.words('english')])
+    if len(removeWords)>0:
+        tweet = ' '.join([word for word in tweet.split() if word not in removeWords])
+    if stem==True:
+        tweet = ' '.join([ps.stem(word) for word in tweet.split()])
+    return tweet
 
 class Operation:
     parents = []
@@ -308,12 +345,123 @@ class Session:
             return
         for i in self.currentSet.children:
             print("Type = ", i.operationType, " parameters = ", i.parameters)
+    
+    ##### Clustering ######
+    
+    # functions for dimension reduction: PCA and UMAP
+    def dimred_PCA(self, docWordMatrix, ndims=25):
+        tsvd = TruncatedSVD(n_components=ndims)
+        tsvd.fit(docWordMatrix)
+        docWordMatrix_pca = tsvd.transform(docWordMatrix)
+        return docWordMatrix_pca
+
+    def dimred_UMAP(self, matrix, ndims=2, n_neighbors=15):
+        umap_2d = umap.UMAP(n_components=ndims, random_state=42, n_neighbors=n_neighbors, min_dist=0.0)
+        proj_2d = umap_2d.fit_transform(matrix)
+        return proj_2d
+
+    # functions for clustering
+    # HDBSCAN
+    # def cluster_hdbscan(points, min_obs):
+    #     hdbscan_fcn = hdbscan.HDBSCAN(min_samples=10, min_cluster_size=min_obs)
+    #     clusters = hdbscan_fcn.fit_predict(points).astype(str)
+    #     return clusters
+
+    # Gaussian Mixure Models
+    def cluster_gmm(self, points, num_clusters):
+        gmm_fcn = GaussianMixture(n_components=num_clusters, random_state=42).fit(points)
+        clusters = gmm_fcn.predict(points).astype(str)
+        return clusters
+
+    # K-Means
+    def cluster_kmeans(self, points, num_clusters):
+        kmean_fcn = KMeans(init='random', n_clusters=num_clusters, random_state=42)
+        clusters = kmean_fcn.fit(points).labels_.astype(str)
+        return clusters
+
+    def make_full_docWordMatrix(self, min_df = 5, inputSet: Subset = None):
+        if (inputSet == None):
+            inputSet = self.currentSet
+        if inputSet.size == 0:
+            return
+        cleanedTweets = []
+        for i in range(self.length):
+            if inputSet.indices[i]:
+                cleanedTweets.append(preProcessingFcn(retrieveRow(i)[15]))
+                print(preProcessingFcn(retrieveRow(i)[15]))
+
+        # create document-word matrix
+        vectorizer = CountVectorizer(strip_accents='unicode', min_df= min_df, binary=False)
+        docWordMatrix_orig = vectorizer.fit_transform(cleanedTweets)
+        docWordMatrix_orig = docWordMatrix_orig.astype(dtype='float64')
+        return docWordMatrix_orig, vectorizer.get_feature_names()
+        # save as sparse document-word matrix as json file
+        rows_orig, cols_orig = docWordMatrix_orig.nonzero()
+        data_orig = docWordMatrix_orig.data
+        docWordMatrix_orig_json = json.dumps({'rows_orig':rows_orig.tolist(), 'cols_orig':cols_orig.tolist(),
+            'data_orig':data_orig.tolist(), 'dims_orig':[docWordMatrix_orig.shape[0], docWordMatrix_orig.shape[1]],
+            'feature_names':vectorizer.get_feature_names()})
+        return docWordMatrix_orig_json
+
+
+    def dimRed_and_clustering(self, docWordMatrix_orig, 
+    dimRed1_method, dimRed1_dims, dimRed2_method, 
+    clustering_when, clustering_method1, num_clusters, min_obs, num_neighbors, inputSet = None):
+        if (inputSet == None):
+            inputSet = self.currentSet
+        # read in document-word matrix
+        data = docWordMatrix_orig.data
+        rows, cols = docWordMatrix_orig.nonzero()
+        dims = docWordMatrix_orig.shape   
+        docWordMatrix = csc_matrix((data, (rows, cols)), shape=(dims[0], dims[1]))
+
+        # do stage 1 dimension reduction
+        if dimRed1_method == 'pca':
+            dimRed1 = self.dimred_PCA(docWordMatrix, docWordMatrix_orig.shape[1])
+        elif dimRed1_method == 'umap':
+            dimRed1 = self.dimred_UMAP(docWordMatrix, docWordMatrix_orig.shape[1])
+        # do stage 2 dimension reduction (if any)
+        if dimRed1_dims > 2:
+            if dimRed2_method == 'pca':
+                dimRed2 = self.dimred_PCA(dimRed1, ndims=2)
+            elif dimRed2_method == 'umap':
+                dimRed2 = self.dimred_UMAP(dimRed1, ndims=2)
+        else:
+            dimRed2 = dimRed1
+        # Clustering
+        # get matrix at proper stage
+        if clustering_when == 'before_stage1':
+            clustering_data = docWordMatrix
+        elif clustering_when == 'btwn':
+            clustering_data = dimRed1
+        elif clustering_when == 'after_stage2':
+            clustering_data = dimRed2
+        # perform clustering
+        if clustering_method1 == 'gmm':
+            clusters = self.cluster_gmm(clustering_data, num_clusters=num_clusters)
+        elif clustering_method1 == 'k-means':
+            clusters = self.cluster_kmeans(clustering_data, num_clusters=num_clusters)
+        # elif clustering_method1 == 'hdbscan':
+        #     clusters = cluster_hdbscan(clustering_data, min_obs=min_obs)
+        # elif clustering_method1 == 'leiden':
+        #     clusters = self.cluster_polis_leiden(clustering_data, num_neighbors=num_neighbors)
+
+
+
+        dimRed_cluster_plot = px.scatter(x= dimRed2[:,0], y= dimRed2[:,1], color= clusters)
+        dimRed_cluster_plot.show()
+        # dimRed_cluster_plot.update_layout(clickmode='event+select')
+        return clusters
 
 # dataSet = None
 # headers = None
 # headerDict = dict()
+
 def retrieveRow(rowNum: int):
     return dataSet[rowNum]
+
+# def retrieveRow(rowNum: int):
+#     return dataSet.iloc[rowNum].values
 
 def createSession(fileName: str) -> Session:
     data = parse_data("allCensus_sample.csv")
@@ -321,6 +469,7 @@ def createSession(fileName: str) -> Session:
     global headers
     global headerDict
     dataSet = data.values
+    #dataSet = data
     headers = data.columns
     headerDict = dict()
     for i in range(len(headers)):
@@ -330,7 +479,7 @@ def createSession(fileName: str) -> Session:
     return s
 
 def test1():
-    s = Session(dataSet)
+    s = createSession("allCensus_sample.csv")
     s.advancedSearch("'covid' and ('hospital' or 'vaccine')")
     #s.printCurrSubset()
     print(s.currentSet.size)
@@ -349,7 +498,7 @@ def test1():
     print(s.currentSet.size)
 
 def test2():
-    s = Session(dataSet)
+    s = createSession("allCensus_sample.csv")
     s.randomSubset(0.01)
     print(s.currentSet.size)
     s.back()
@@ -376,7 +525,7 @@ def test2():
     s.next()
 
 def test3():
-    s = Session(dataSet)
+    s = createSession("allCensus_sample.csv")
     s.randomSubset(0.001)
     print(s.currentSet.size)
     tempSet = s.currentSet
@@ -390,7 +539,7 @@ def test3():
     print(s.currentSet.size)
 
 def test4():
-    s = Session(dataSet)
+    s = createSession("allCensus_sample.csv")
     s.searchKeyword(["the", "Census"])
     print(s.currentSet.size)
     s.back()
@@ -425,7 +574,7 @@ def test4():
     print(s.currentSet.size)
     
 def test5():
-    s = Session(dataSet)
+    s = createSession("allCensus_sample.csv")
     s.simpleRandomSample(17000)
     #s.printCurrSubset()
     print(s.currentSet.size)
@@ -435,7 +584,7 @@ def test5():
     print(s.currentSet.size)
 
 def test6():
-    s = Session(dataSet)
+    s = createSession("allCensus_sample.csv")
     s.simpleRandomSample(30)
     s.printCurrSubset()
     print("\n\n ---------------------------------------------------------- \n")
@@ -464,6 +613,19 @@ def test7(): #same as test 1
     s.regexSearch("trump")
     print(s.currentSet.size)
 
+def test8():
+    s = createSession("allCensus_sample.csv")
+    s.filterBy("State", "New Jersey")
+    s.regexSearch("Trump")
+    print(s.currentSet.size)
+    #s.printCurrSubset()
+    matrix, words = s.make_full_docWordMatrix(min_df= 1)
+    print(words)
+    print(matrix)
+    test = s.dimRed_and_clustering(matrix, 'pca', 2, 'umap', 'btwn', 'gmm', 25, 2, 25)
+    print(test)
+    #print(matrix)
+
 if __name__=='__main__':
     # test = parse_data("allCensus_sample.csv")
     # dataSet = test.values
@@ -471,7 +633,7 @@ if __name__=='__main__':
     # for i in range(len(headers)):
     #     colName = headers[i]
     #     headerDict[colName] = i
-    test7()
+    test8()
 
 
 
