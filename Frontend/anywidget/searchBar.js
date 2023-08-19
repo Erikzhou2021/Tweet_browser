e1e1bf0ce2dcd15e08a91d86f74191984afa91c5
erzh <erzh@umich.edu>
2023-08-19 17:58:21 -0400

    Refactored css to use flexboxes

--- Frontend/anywidget/searchBar.js
+++ Frontend/anywidget/searchBar.js
@@ -1,5 +1,6 @@
-export function render({ model, el }) {        
-    el.classList.add("search-bar");
+export function render({ model, el }) {   
+    let inputBar = document.createElement("div");
+    inputBar.classList.add("search-bar");
     let input = document.createElement('input');
     input.value = 'test';
     input.classList.add("search");
@@ -10,13 +11,15 @@ export function render({ model, el }) {
     plusButton.addEventListener("click", function(){ addValue(input.value); });
     
     let list = document.createElement('div');
+    list.classList.add("value-list");
+    let closeTag = '<span class="close">x</span>';
 
     function addValue(value){
         if(value == ""){
             return;
         }
         for(let index = 0; index < list.children.length; index++){
-            if(list.children.item(index).innerHTML == value + '  <span class="close">x</span>'){
+            if(list.children.item(index).innerHTML == value + closeTag){
                 return;
             }
         }
@@ -26,19 +29,32 @@ export function render({ model, el }) {
 
         let tempText = document.createElement('div');
         tempText.innerHTML = value;
-        tempText.classList.add("searchedValue");
-        tempText.innerHTML = tempText.innerHTML + '&ensp; <span class="close">x</span>';
+        tempText.classList.add("searched-value");
+        tempText.innerHTML = tempText.innerHTML + closeTag;
         list.appendChild(tempText);
         var closebtns = document.getElementsByClassName("close");
 
         for (let i = 0; i < closebtns.length; i++) {
           closebtns[i].addEventListener("click", function() {
+            let deletedVal = this.parentElement.innerHTML;
+            deletedVal = deletedVal.substring(0, deletedVal.length - closeTag.length);
+            let oldVal = model.get("value");
+            for(let i = 0; i < oldVal.length; i++){
+                if(oldVal[i] == deletedVal){
+                    oldVal[i] = oldVal[oldVal.length -1];
+                    oldVal.pop();
+                    model.set("value", oldVal);
+                    model.save_changes();
+                    break;
+                }
+            }
             this.parentElement.remove();
           });
         }
     }
     
-    el.appendChild(input);
-    el.appendChild(plusButton);
+    inputBar.appendChild(input);
+    inputBar.appendChild(plusButton);
+    el.appendChild(inputBar);
     el.appendChild(list);
 }
