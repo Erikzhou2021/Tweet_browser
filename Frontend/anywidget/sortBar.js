f7fce831b67b3dbb2de04c7d95e1233b896eae97
erzh <erzh@umich.edu>
2023-08-24 22:00:11 -0400

    Tried to implement sorting

--- Frontend/anywidget/sortBar.js
+++ Frontend/anywidget/sortBar.js
@@ -3,9 +3,19 @@ export function render({ model, el }) {
     let sort = document.createElement("div");
     let temp = document.createElement("div");
     let sorted = document.createElement("input");
+    sorted.addEventListener("change", updateSorted);
     sorted.setAttribute("type", "checkbox");
     sorted.classList.add("checkbox-round");
     temp.appendChild(sorted);
+
+    function updateSorted(){
+        let sortEnabled = 0;
+        if (sorted.checked){
+            sortEnabled = 1;
+        }
+        model.set("sortEnabled", sortEnabled);
+        model.save_changes();
+    }
     
 
     let dropDowns = document.createElement("div");
@@ -19,6 +29,13 @@ export function render({ model, el }) {
     let dropDown1 = document.createElement("select");
     addOption(dropDown1, "Displayed Examples");
     addOption(dropDown1, "Entire Dataset");
+    dropDown1.addEventListener("change", updateSortScope);
+
+    function updateSortScope(){
+        let sortScope = dropDown1.value;
+        model.set("sortScope", sortScope);
+        model.save_changes();
+    }
 
     let byText = document.createElement("strong");
     byText.innerHTML = "&nbsp; By &nbsp;";
@@ -29,6 +46,24 @@ export function render({ model, el }) {
     addOption(dropDown2, "Retweets");
     addOption(dropDown2, "Username");
 
+    dropDown2.addEventListener("change", updateSortColumn);
+
+    function updateSortColumn(){
+        let sortColumn = dropDown2.value;
+        if (sortColumn == "Geogrpahy"){
+            sortColumn = "State";
+        }
+        if (sortColumn == "Date"){
+            sortColumn = "CreatedTime";
+        }
+        if (sortColumn == "Username"){
+            sortColumn = "SenderScreenName";
+        }
+        
+        model.set("sortColumn", sortColumn);
+        model.save_changes();
+    }
+
     dropDowns.appendChild(dropDown1);
     dropDowns.appendChild(byText);
     dropDowns.appendChild(dropDown2);
@@ -37,9 +72,22 @@ export function render({ model, el }) {
     let orderBy = document.createElement("div");
     let asc = document.createElement("img");
     asc.src = "images/ascending.svg";
+    asc.addEventListener("click", updateAsc);
+
+    function updateAsc(){
+        model.set("sortOrder", "ASC");
+        model.save_changes();
+    }
 
     let dsc = document.createElement("img");
     dsc.src = "images/descending.svg";
+    dsc.addEventListener("click", updateDsc);
+
+    function updateDsc(){
+        model.set("sortOrder", "DSC");
+        model.save_changes();
+    }
+
     orderBy.appendChild(asc);
     orderBy.appendChild(dsc);
 
