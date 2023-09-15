e5ce08daf867e4e9d730f775ae328503e0fab04f
Erik Zhou <erikzhou2021@gmail.com>
2023-09-15 17:56:22 -0400

    Fixed more sorting issues

--- Frontend/anywidget/sortBar.js
+++ Frontend/anywidget/sortBar.js
@@ -3,8 +3,6 @@ export function render({ model, el }) {
     let sort = document.createElement("div");
     let temp = document.createElement("div");
 
-    
-
     let dropDowns = document.createElement("div");
     let label = document.createElement("strong");
     label.innerHTML = "&nbsp;Sort";
@@ -12,11 +10,11 @@ export function render({ model, el }) {
     sort.appendChild(label);
     sort.classList.add("box-container");
 
-
     let dropDown1 = document.createElement("select");
-    addOption(dropDown1, "Displayed Examples");
-    addOption(dropDown1, "Entire Dataset");
+    addOption(dropDown1, "Displayed Examples", "Displayed Examples");
+    addOption(dropDown1, "Entire Dataset", "Entire Dataset");
     dropDown1.addEventListener("change", updateSortScope);
+    dropDown1.value = model.get("sortScope");
 
     function updateSortScope(){
         let sortScope = dropDown1.value;
@@ -28,26 +26,16 @@ export function render({ model, el }) {
     byText.innerHTML = "&nbsp; By &nbsp;";
 
     let dropDown2 = document.createElement("select");
-    addOption(dropDown2, "None");
-    addOption(dropDown2, "Date");
-    addOption(dropDown2, "Geography");
-    addOption(dropDown2, "Retweets");
-    addOption(dropDown2, "Username");
-
+    addOption(dropDown2, "None", "None");
+    addOption(dropDown2, "Date", "CreatedTime");
+    addOption(dropDown2, "Geography", "State");
+    addOption(dropDown2, "Retweets", "Retweets");
+    addOption(dropDown2, "Username", "SenderScreenName");
     dropDown2.addEventListener("change", updateSortColumn);
+    dropDown2.value = model.get("sortColumn");
 
     function updateSortColumn(){
         let sortColumn = dropDown2.value;
-        if (sortColumn == "Geogrpahy"){
-            sortColumn = "State";
-        }
-        if (sortColumn == "Date"){
-            sortColumn = "CreatedTime";
-        }
-        if (sortColumn == "Username"){
-            sortColumn = "SenderScreenName";
-        }
-        
         model.set("sortColumn", sortColumn);
         model.save_changes();
     }
@@ -79,10 +67,10 @@ export function render({ model, el }) {
     orderBy.appendChild(asc);
     orderBy.appendChild(dsc);
 
-    function addOption(parent, value, css = ""){
+    function addOption(parent, text, value, css = ""){
         let temp = document.createElement("option");
         temp.value = value;
-        temp.innerHTML = value;
+        temp.innerHTML = text;
         if(css != ""){
             temp.classList.add(css);
         }
