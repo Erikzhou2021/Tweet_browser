bb10be2ece5c3715928295a28417d7d200df9696
Erik Zhou <erikzhou2021@gmail.com>
2023-10-06 22:28:21 -0400

    Made AI summary interactive

--- Frontend/anywidget/aiSummary.js
+++ Frontend/anywidget/aiSummary.js
@@ -6,7 +6,20 @@ export function render({ model, el }) {
     tip.innerHTML = "Click each sentence to view the contributing tweets";
     let summary = document.createElement("div");
     summary.classList.add("summary");
-    summary.innerHTML = "Blah blah blah blah";
+    let sentences = model.get("value");
+
+    function showcontributing(){
+        model.set("selected", this.dataset.num);
+        model.save_changes();
+    }
+
+    for(let i = 0; i < sentences.length; i++){
+        let temp = document.createElement("span");
+        temp.innerHTML = sentences[i];
+        temp.dataset.num = i;
+        temp.addEventListener("click", showcontributing);
+        summary.appendChild(temp);
+    }
     let caveat = document.createElement("h3");
     caveat.innerHTML = "*Not all tweets are captured in the summary. Click to view ommited tweets"
     el.appendChild(header);
