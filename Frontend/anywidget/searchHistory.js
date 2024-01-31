31f9459e033a7087102fe4a633d2f064f851a6d0
Erik Zhou <erikzhou2021@gmail.com>
2024-01-31 12:02:19 -0500

    Minor bugfixes / update for docker

--- Frontend/anywidget/searchHistory.js
+++ Frontend/anywidget/searchHistory.js
@@ -1,26 +1,27 @@
 export function render({ model, el }) { 
-    let filePath = model.get("filePath");  
-    el.classList.add("search-history");  
+    return;
+    // let filePath = model.get("filePath");  
+    // el.classList.add("search-history");  
 
-    let left = document.createElement("img");
-    left.src = filePath + "left.svg";
-    left.classList.add("arrow");
-    let leftText = document.createElement("span");
-    leftText.innerHTML = "Prev";
+    // let left = document.createElement("img");
+    // left.src = filePath + "left.svg";
+    // left.classList.add("arrow");
+    // let leftText = document.createElement("span");
+    // leftText.innerHTML = "Prev";
 
-    let middle = document.createElement("div");
-    middle.innerHTML = "1";
-    middle.classList.add("middle");
+    // let middle = document.createElement("div");
+    // middle.innerHTML = "1";
+    // middle.classList.add("middle");
 
-    let right = document.createElement("img");
-    right.src = filePath + "right.svg";
-    right.classList.add("arrow");
-    let rightText = document.createElement("span");
-    rightText.innerHTML = "Next";
+    // let right = document.createElement("img");
+    // right.src = filePath + "right.svg";
+    // right.classList.add("arrow");
+    // let rightText = document.createElement("span");
+    // rightText.innerHTML = "Next";
 
-    el.appendChild(left);
-    el.appendChild(leftText);
-    el.appendChild(middle);
-    el.appendChild(rightText);
-    el.appendChild(right);
+    // el.appendChild(left);
+    // el.appendChild(leftText);
+    // el.appendChild(middle);
+    // el.appendChild(rightText);
+    // el.appendChild(right);
 }
