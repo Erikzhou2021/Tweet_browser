ae880fdfe0656e642760ecbb04f69cd34b80c112
Erik Zhou <erikzhou2021@gmail.com>
2024-08-30 10:48:28 -0400

    Style changes

--- Frontend/anywidget/datasetDisplay.js
+++ Frontend/anywidget/datasetDisplay.js
@@ -2,26 +2,21 @@ export function render({ model, el }) {
     el.classList.add("dataset-display");
     let leftText = document.createElement("div");
 
-    let text = document.createElement("strong");
-    text.innerHTML = "Dataset: " + tweetNumber();
-    text.classList.add("large-font");
-
-    let fileName = document.createElement("div");
-    fileName.innerHTML = model.get("fileName");
-    fileName.classList.add("small-font");
-
-    leftText.appendChild(text);
-    leftText.appendChild(fileName);
-    leftText.classList.add("text-block");
+    let fileName = model.get("fileName");
+    if(fileName == null || fileName == ""){
+        fileName = "No File Loaded";
+    }
+    let filePath = model.get("filePath");
+    let fileIcon = document.createElement("img");
+    fileIcon.src = filePath + "file.svg";
+    
+    leftText.innerHTML = "&nbsp; <b><u>" + fileName + "</u></b> &nbsp; " + model.get("size") + " Posts";
 
     let changeButton = document.createElement("label");
     changeButton.innerHTML = '<input type="file" id="fileup">Change Dataset'
     changeButton.classList.add("change-input");
 
-    function tweetNumber(){ // might need to display different for large numbers e.g. "1.3 million"
-        return model.get("size");
-    }
-
+    el.appendChild(fileIcon);
     el.appendChild(leftText);
-    el.appendChild(changeButton);
+    // el.appendChild(changeButton);
 }
