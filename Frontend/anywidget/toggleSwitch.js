7ee7b610fa818d28289316bcd5860ed86f31bdde
Erik Zhou <erikzhou2021@gmail.com>
2024-09-02 15:30:40 -0400

    Styling filter bar

--- Frontend/anywidget/toggleSwitch.js
+++ Frontend/anywidget/toggleSwitch.js
@@ -34,12 +34,4 @@ export function render({ model, el }) {
         }
     }
 
-    const query = '.date-constraint > input:first-of-type';
-    let start = model.get("calendarStart");
-    let end = model.get("calendarEnd");
-    let results = document.querySelectorAll(query);
-    results.forEach((calenderEl) => {
-        calenderEl.setAttribute('min', start);
-        calenderEl.setAttribute('max', end);
-    });
 }
