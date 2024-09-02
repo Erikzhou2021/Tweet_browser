b93d33a975c7999b163e543fa82f24b0e6a37c73
Erik Zhou <erikzhou2021@gmail.com>
2024-09-02 15:43:36 -0400

    Filter bar styling again

--- Frontend/anywidget/toggleSwitch.js
+++ Frontend/anywidget/toggleSwitch.js
@@ -2,7 +2,7 @@ export function render({ model, el }) {
     el.classList.add("toggle-switch");
     let label = model.get("label");
     let text = document.createElement("div");
-    text.innerHTML = "<h4> " + label + "<h4/>";
+    text.innerHTML = label;
     let toggleSwitch = document.createElement("label");
     toggleSwitch.classList.add("switch");
     let invisibleBox = document.createElement("input");
