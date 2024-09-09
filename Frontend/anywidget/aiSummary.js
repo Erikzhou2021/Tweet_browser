0ce7f4643228b3145f75299bb91d5290637c23cb
Erik Zhou <erikzhou2021@gmail.com>
2024-09-09 19:37:58 -0400

    AI summary frontend start

--- Frontend/anywidget/aiSummary.js
+++ Frontend/anywidget/aiSummary.js
@@ -1,12 +1,11 @@
 export function render({ model, el }) {      
     el.classList.add("ai-summary"); 
     let header = document.createElement("h1");
-    header.innerHTML = "AI Summary of Displayed Tweets";
+    header.innerHTML = "AI Generated Summary";
     let tip = document.createElement("h3");
     tip.innerHTML = "Click each sentence to view the contributing tweets";
     let summary = document.createElement("div");
     summary.classList.add("summary");
-    let sentences = model.get("value");
 
     function showcontributing(){
         model.set("selected", this.dataset.num);
@@ -15,13 +14,18 @@ export function render({ model, el }) {
         model.save_changes();
     }
 
-    for(let i = 0; i < sentences.length; i++){
-        let temp = document.createElement("span");
-        temp.innerHTML = sentences[i];
-        temp.dataset.num = i;
-        temp.addEventListener("click", showcontributing);
-        summary.appendChild(temp);
+    function renderSentences(){
+        let sentences = model.get("value");
+        for(let i = 0; i < sentences.length; i++){
+            let temp = document.createElement("span");
+            temp.innerHTML = sentences[i];
+            temp.dataset.num = i;
+            temp.addEventListener("click", showcontributing);
+            summary.appendChild(temp);
+        }
     }
+    
+    model.on("change:value", renderSentences);
     let caveat = document.createElement("h3");
     caveat.innerHTML = "*Not all tweets are captured in the summary. Click to view ommited tweets"
     el.appendChild(header);
