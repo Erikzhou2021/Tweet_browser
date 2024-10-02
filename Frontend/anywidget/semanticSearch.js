8064b66a0013c4ffba59f1d4a31f3d6fd05bba74
Erik Zhou <erikzhou2021@gmail.com>
2024-10-01 20:00:29 -0400

    Started semantic search slider

--- Frontend/anywidget/semanticSearch.js
+++ Frontend/anywidget/semanticSearch.js
@@ -8,13 +8,31 @@ export function render({ model, el }) {
     input.classList.add("semantic-search");
 
     let fullHeader = document.createElement("div");
+    fullHeader.classList.add("semantic-search-title");
     let header = document.createElement("h4");
     header.innerHTML = "Semantic Match";
     fullHeader.appendChild(header);
     let header2 = document.createElement("h5");
     header2.innerHTML = "(posts are about)";
     fullHeader.appendChild(header2);
-    fullHeader.classList.add("full-header");
+    let slider = document.createElement("input");
+    slider.type = "range";
+    slider.min = "1";
+    slider.max = "100";
+    slider.classList.add("slider");
+    fullHeader.appendChild(slider);
+    let firstText = document.createElement("div");
+    firstText.innerHTML = "Top ";
+    let intBox = document.createElement("input");
+    intBox.classList.add("int-box");
+    intBox.min = "1";
+    intBox.max = "100";
+    intBox.value = 50;
+    let secondText = document.createElement("div");
+    secondText.innerHTML = "% Most Relavent";
+    fullHeader.appendChild(firstText);
+    fullHeader.appendChild(intBox);
+    fullHeader.appendChild(secondText);
 
     input.addEventListener("change", (event) => {
         model.set("value", input.value);
